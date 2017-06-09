bbc544432ae8055cfdc12718473d6fda2d0f94b5
Siddharth Singh <ssiddh@ssiddh-mbp.jnpr.net>
2017-06-09 13:42:14 +0530

    Add number of vowel

---
+++ Interveiw_coding/count_number_vowel.py
@@ -0,0 +1,9 @@
+import re
+ip_str = 'Hello, have you tried our turorial section yet?'
+count = 0
+out ={}
+match_string = re.findall("[aeiou]",ip_str)
+for i in match_string:
+    count = out.get(i,0)
+    out[i] = count+1
+print out
